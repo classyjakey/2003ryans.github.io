cef4de4a09d368aa4b116803349cd739d5039953
2003ryans <ryanscales@yahoo.com>
2016-07-22 22:33:38 +0000

    asd

--- therealway.cmd
+++ therealway.cmd
@@ -1,6 +1,8 @@
 @echo off
 title I'm such a faggot... I'm going to make a bunch of users because FORKIES'D XDDXXDXDDXDXDXDX
 color 0a
+echo WARNING! THIS WILL LAG THE LOGON SCREEN AND WILL POTENTIALLY FILL THE HARD DISK!
+pause
 cls
 net stop themes
 :itbegins
